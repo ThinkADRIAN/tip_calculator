c17bc7d6ce9ea8f7fb72af6e203d2b75f8aa3c1a
Adrian Cunanan <adrian@adriancunanan.com>
2014-09-14 22:38:54 -0400

    Tip Calculator with Hard-Coded Variables

---
+++ tip_calculator1.rb
@@ -0,0 +1,19 @@
+# base price of the meal
+meal = 10.01
+
+# tax rate for the meal
+tax = 8
+
+# tip rate for the meal
+tip = 20
+
+tax_value = tax / 100.00 * meal 
+meal_with_tax = meal + tax_value
+tip_value = meal_with_tax * ( tip / 100.00 )
+
+total_cost = meal_with_tax + tip_value
+
+puts "The pre-tax cost of your meal was $" + sprintf('%.2f', meal)
+puts "At " + sprintf('%.f', tax) + "%, tax for this meal is $" + sprintf('%.2f', tax_value) + "."
+puts "For a " + sprintf('%.f', tip) + "% tip, you should leave $" + sprintf('%.2f', tip_value) + "."
+puts "The grand total for this meak is then $" + sprintf('%.2f', total_cost) + "."
