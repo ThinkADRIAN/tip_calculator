2ffc725fc43eac005a8b9e150e82a1491d7b4101
Adrian Cunanan <adrian@adriancunanan.com>
2014-09-14 22:50:15 -0400

    Tip Calulator with User Supplied Variables

---
+++ tip_calculator2.rb
@@ -0,0 +1,25 @@
+# base price of the meal
+puts "Enter the cost of your meal before tax:"
+meal_s = gets
+meal = Float(meal_s)
+
+# tax rate for the meal
+puts "Enter the tax in percentage"
+tax_s = gets
+tax = Float(tax_s)
+
+# tip rate for the meal
+puts "Enter the tip in percentage"
+tip_s = gets
+tip = Float(tip_s)
+
+tax_value = tax / 100.00 * meal 
+meal_with_tax = meal + tax_value
+tip_value = meal_with_tax * ( tip / 100.00 )
+
+total_cost = meal_with_tax + tip_value
+
+puts "The pre-tax cost of your meal was $" + sprintf('%.2f', meal)
+puts "At " + sprintf('%.f', tax) + "%, tax for this meal is $" + sprintf('%.2f', tax_value) + "."
+puts "For a " + sprintf('%.f', tip) + "% tip, you should leave $" + sprintf('%.2f', tip_value) + "."
+puts "The grand total for this meak is then $" + sprintf('%.2f', total_cost) + "."
